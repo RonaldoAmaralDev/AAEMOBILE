2b2f431a717b960589de0a8f35e91948bfbca445
Ronaldo Gonçalves Amaral <ronaldocotemig@gmail.com>
2019-08-28 16:11:50 -0300

    Start commit

---
+++ app/src/main/java/com/delaroystudios/uploadmedia/rota/PostosCombustivel.java
@@ -0,0 +1,15 @@
+package com.delaroystudios.uploadmedia.rota;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.delaroystudios.uploadmedia.R;
+
+public class PostosCombustivel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postos_combustivel);
+    }
+}
